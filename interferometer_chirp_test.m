8c382c72519d5205b4290258a9671bc7aa1e4ca9
gambhirranjit <134450916+gambhirranjit@users.noreply.github.com>
2023-12-08 15:28:24 -0800

    Update interferometer_chirp_test.m

--- interferometer_chirp_test.m
+++ interferometer_chirp_test.m
@@ -1,12 +1,18 @@
+%% **************************************************************************************************
+% * This code read the interferometer signal data from the file "interferometer_chirp_test.mat"     *
+% * smooth the inteferometer signal, find the peaks' locations, calculateFSR for the given path     *
+% * difference (adjustable mirror position) and calculate the chirp infrequency in the pulse width  *
+%%***************************************************************************************************
+
 clear all;
 close all;
 
 
 
 
-mode = 'one';
-posVec = [40, 40, 40, 40, 40]; %[421, 321, 221, 121, 40]; %[40, 40, 40, 40, 40]; %
-if strcmp(mode, 'one')
+mode = 'chirp'; % enter the FSR for FSR simulation and chirp for chirp test
+posVec = 40; 
+if strcmp(mode, 'chirp')
         
     data = load ('interferometer_chirp_test.mat');
     TIME_RAW = data.TIME;
@@ -14,42 +20,50 @@ if strcmp(mode, 'one')
 
     for i = 1:1
     
-    pathDiff = (2 .* (452.35 - posVec(i))) .*1E-3;
+    pathDiff = (2 .* (452.35 - posVec)) .*1E-3;
     idx_START = find (TIME_RAW > 10E-6, 1, 'first');
     TIME = TIME_RAW(1:end);
     SIGNAL = SIGNAL_RAW(1:end);
     %inputs = [time, signal, breakPoint, MinPeakDistShort, MinPeakDistLong, MinPeakHeightShort, MinPeakHeightLong, pathDiff
     [time1, signal1, FSR] =  peakfit(TIME, SIGNAL, 1.5E-4, 30, 450, 0.056, 0.0695, pathDiff);
-
     end
   
 
-
-
 else
     c = 3E8;   
-    lambda_center = 935.8388E-9;
-    lambda = linspace(1, 1.00001, 5000) .* lambda_center;
-    delta_x = 110E-3;
-    y = cos(2*pi./lambda .* delta_x);
-    [pks, locs] = findpeaks (y, 'minPeakHeight', 0.9);
-    
-    figure(3)
-    plot(lambda .* 1E9, y, '-')
-    hold on;
-    plot(lambda(locs).*1E9, pks, '.', 'markersize', 25)
-    hold on;
-    delta_lambda = diff(lambda(locs));
-    delta_frequency = 3E8/(lambda_center).^2 * delta_lambda;
-    total_delta_lambda_shift = delta_frequency * 20/1E9;
+    lambda_center = 935.8388E-9; %
+    lambda = linspace(0.99997, 1.0000, 5000) .* lambda_center;
+    posVec = [420, 370, 320];
+    for i = 1:3
+        delta_x = (2 .* (452.35 - posVec(i))) .* 1E-3;
+        y = 2 - 2 .* cos(2*pi./lambda .* delta_x);
+        y_normalized = (y./max(y)).^2;
+        [pks, locs] = findpeaks (y_normalized, 'minPeakHeight', 0.9);
+        lambda_locs = lambda(locs) .* 1E9;
+        figure(i)
+        plot(lambda .* 1E9, y_normalized, '-', 'linewidth', 2)
+        hold on;
+        plot(lambda_locs, pks, '.', 'markersize', 35)
+        xline(lambda_locs(1), '--','linewidth', 2);
+        xline(lambda_locs(2), '--', 'linewidth',2);
+        xlabel('\lambda(nm)');
+        ylabel('interference signal-normalized')
+        ylim([0,1.1]);
+        hold on;
+        grid on;
+        grid minor;
+        delta_lambda = lambda_locs(2) - lambda_locs(1)
+%         delta_frequency = 3E8/(lambda_center).^2 * delta_lambda;
+%         total_delta_lambda_shift = delta_frequency * 20/1E9;
+    end
 end
 
 function [fit_time, fit_dLambda, FSR] = peakfit(xIn_raw, yIn_raw, breakPoint, MinPeakDistShort, MinPeakDistLong, MinPeakHeightShort, MinPeakHeightLong, pathDiff)
-    idx_startPoint = find (xIn_raw > 0, 1, 'first');
+    idx_startPoint = find (xIn_raw > 10E-6, 1, 'first');
     xIn = xIn_raw(idx_startPoint:end);
     yIn = yIn_raw(idx_startPoint:end);
     wavelength = 935E-9;
-    FSR = wavelength.^2/(pathDiff)./1E-9
+    FSR = wavelength.^2/(pathDiff)./1E-9;
     figure(1)
     plot(xIn, yIn, '-')
     grid on;
@@ -57,7 +71,7 @@ function [fit_time, fit_dLambda, FSR] = peakfit(xIn_raw, yIn_raw, breakPoint, Mi
     xlabel('time(s)');
     ylabel('interferometer signal (V)');
     hold on;
-    [yIn_smooth,window] = smoothdata(yIn,"gaussian", 150);
+    [yIn_smooth] = smoothdata(yIn,"gaussian", 150);
     plot(xIn, yIn_smooth, '-')
     hold on;
     idx_first_segment = find(xIn < breakPoint, 1,'last');
@@ -73,27 +87,29 @@ function [fit_time, fit_dLambda, FSR] = peakfit(xIn_raw, yIn_raw, breakPoint, Mi
     plot(time_pks, pks, '.', 'markersize', 20)
     hold on;
 
-    idx_plot = find(time_pks > 10E-6, 1, 'first'); %60E-6  2E-7
 
-    
-    delta_time = time_pks(idx_plot:end); % - time_pks(1); %diff(time_pks);
+    delta_time = time_pks(1:end); % - time_pks(1); % - time_pks(1); %diff(time_pks);
     figure(3)
     delta_lambda = linspace(1, length(delta_time), length(delta_time));
-    fit_time = delta_time(1:end-1);
+    fit_time = delta_time(1:end-1); %delta_time(1:end-1);
     fit_dLambda = (delta_lambda(1:end-1)-1).* FSR;
-    plot((delta_time(1:end-1)), fit_dLambda, '.r', 'markersize', 20)
+    plot((fit_time), fit_dLambda, '.r', 'markersize', 20)
     grid on;
     grid minor;
     xlabel('time(s)');
     ylabel('\Delta\lambda(nm)');
     hold on;
+
+    tq = 0:1E-6: 1.6E-3;
+    sp = pchip(fit_time, fit_dLambda, tq);
+    plot(tq(100:end), sp(100:end), '--', 'linewidth', 2);
+    hold on;
+    idx_1p5ms = find(tq > 1.5E-3, 1, 'first');
+    sp(idx_1p5ms);
+    plot(tq(idx_1p5ms), sp(idx_1p5ms), '.b', 'markersize', 35)
+    hold on;
+    yline(sp(idx_1p5ms), '--', 'linewidth', 2);
     
 
 end
-function [xOut, yOut] = stitchData(xIn, yIn)
-    [yIn_smooth,window] = smoothdata(yIn,"gaussian", 100);
-    [pks,locs] = findpeaks(yIn_smooth(floor(length(xIn)/2):end), 'MinPeakHeight', 0.06, 'MinPeakDistance', 1000, 'MinPeakProminence',0.01);
-    figure(10)
-    plot(xIn, yIn, '-')
-    hold on;
-end
+
